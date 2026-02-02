0ea553fd96e9be4a09effe0b0e32b5a75e4f4936
saptarshimandal428@gmail.com <saptarshimandal428@gmail.com>
2026-02-03 01:45:44 +0530

    error fixed

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose"); // import mongoose
+const { Schema } = mongoose; // extract Schema
 
 const listingSchema = new Schema({
     title: {
@@ -7,15 +7,24 @@ const listingSchema = new Schema({
         required: true,
     },
     description: String,
-    Image: {
-        type: String,
-        default: "https://images.unsplash.com/photo-1768409234914-96f61529b7e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1768409234914-96f61529b7e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default:
+                "https://images.unsplash.com/photo-1768409234914-96f61529b7e2?q=80&w=2070&auto=format&fit=crop",
+            set: (v) =>
+                v === ""
+                    ? "https://images.unsplash.com/photo-1768409234914-96f61529b7e2?q=80&w=2070&auto=format&fit=crop"
+                    : v,
+        },
     },
     price: Number,
     location: String,
     country: String,
 });
 
-const Listing = mongoose.model("Listing", listingSchema);
-module.exports = Listing;
+module.exports = mongoose.model("Listing", listingSchema); // export the model
