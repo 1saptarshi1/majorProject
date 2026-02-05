dd1d4a6355590f47808ee251e9423ceca64d4605
saptarshimandal428@gmail.com <saptarshimandal428@gmail.com>
2026-02-05 20:01:10 +0530

    Custom Error Handling

--- app.js
+++ app.js
@@ -49,10 +49,14 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 // Create route
-app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+app.post("/listings", async (req, res, next) => {
+    try{
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    }catch(err) {
+        next(err);
+    }
 
 });
 
@@ -92,6 +96,10 @@ app.delete("/listings/:id", async (req, res) => {
 //     console.log("sample was saved");
 //     res.send("succssful testing");
 // });
+
+app.use((err, req, res, next) => {
+    res.send("something went wrong!");
+});
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
