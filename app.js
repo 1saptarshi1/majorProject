2d26e4826c322cd5c68bc555358a473a9585c44a
saptarshimandal428@gmail.com <saptarshimandal428@gmail.com>
2026-02-05 20:16:57 +0530

    Add wrapAsync

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-
+const wrapAsync = require("./utils/wrapAsyc.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -49,16 +49,12 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 // Create route
-app.post("/listings", async (req, res, next) => {
-    try{
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    }catch(err) {
-        next(err);
-    }
-
-});
+app.post("/listings", wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+);
 
 // Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
