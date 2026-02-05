f2bad22fdb1cf88a404c761f44ecac5dba7c4a7b
saptarshimandal428@gmail.com <saptarshimandal428@gmail.com>
2026-02-05 23:01:06 +0530

    Validation for Schema

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsyc.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { error } = require("console");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -52,8 +53,10 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 
 // Create route
 app.post("/listings", wrapAsync(async (req, res, next) => {
-    if (!req.body.listing){
-        throw new ExpressError(400, "Send valid data for listing");
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+        throw new ExpressError(400, result.error);
     }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
